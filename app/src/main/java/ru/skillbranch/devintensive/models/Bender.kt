db10f639a7cba9f13bfbb640f61900b89030e1da
Deep27 <geep2795@gmail.com>
2020-05-01 21:43:08 +0300

    Try again.

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -64,7 +64,7 @@ class Bender(
             "Как меня зовут?", listOf("Бендер", "Bender"),
             listOf(
                 Pair(
-                    { answer -> !answer.isEmpty() && answer[0].isUpperCase() || answer.isEmpty() },
+                    { answer -> !answer.isEmpty() && answer[0].isUpperCase() || answer.length > 0 },
                     "Имя должно начинаться с заглавной буквы"
                 )
             )
@@ -74,7 +74,7 @@ class Bender(
         PROFESSION(
             "Назови мою профессию?", listOf("сгибальщик", "bender"), listOf(
                 Pair(
-                    { answer -> !answer.isEmpty() && answer[0].isLowerCase() || answer.isEmpty() },
+                    { answer -> !answer.isEmpty() && answer[0].isLowerCase() || answer.length > 0 },
                     "Профессия должна начинаться со строчной буквы"
                 )
             )
