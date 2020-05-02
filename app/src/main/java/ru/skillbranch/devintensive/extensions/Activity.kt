435f51a24380f61485e284e60a8faf87624cc9ee
Deep27 <geep2795@gmail.com>
2020-05-02 22:24:07 +0300

    Playing with isKeyboardOpen.

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,24 +2,28 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToInt
 
 fun Activity.hideKeyboard() {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
 }
 
-//fun Activity.getRootView(): View = findViewById(android.R.id.content);
+fun Activity.getRootView(): View = findViewById(android.R.id.content);
 
-//fun Activity.isKeyboardOpen(): Boolean {
-//    val visibleBounds = Rect()
-//    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
-//    val heigthDiff = getRootView().height - visibleBounds.height()
-//    val marginOfError = convertDpToPx(this, 10f).roundToInt()
-//    return heigthDiff > marginOfError
-//}
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val heigthDiff = getRootView().height - visibleBounds.height()
+    val marginOfError = convertDpToPx(this, 10f).roundToInt()
+    return heigthDiff > marginOfError
+}
 
-//fun Activity.isKeyboardClosed(): Boolean = !this.isKeyboardOpen()
+fun Activity.isKeyboardClosed(): Boolean = !this.isKeyboardOpen()
 
-//private fun convertDpToPx(context: Context, dp: Float): Float =
-//    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+private fun convertDpToPx(context: Context, dp: Float): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
